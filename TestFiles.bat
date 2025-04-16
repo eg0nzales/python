3f1cc16e5a4d997f80a2016ae1de786565ffc074
eg0nzales <egonzales@nrchealth.com>
2025-04-16 15:27:41 -0400

    Create TestFiles.bat

---
+++ TestFiles.bat
@@ -0,0 +1,6 @@
+@echo off
+curl -L -O https://raw.githubusercontent.com/eg0nzales/python/main/POST_TEST_syndicate
+del POST_TEST_syndicate.py
+ren POST_TEST_syndicate POST_TEST_syndicate.py
+python POST_TEST_syndicate.py
+pause
